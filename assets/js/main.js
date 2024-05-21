61ba0343cba9e012041ee72aa66441196d9b81f2
Ashraful <ashrafulkarim.dev@gmail.com>
2024-05-21 22:49:05 +0600

    banner complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,10 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
+
+// slider
+var swiper = new Swiper(".testimonial", {
+    slidesPerView: 1,
+    direction: 'horizontal',
+    loop: true,
+});
